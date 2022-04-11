d51c42246b11e5e4d4c42563babd72af898a2d2d
byungchan <icoo08216@gmail.com>
2022-04-11 20:09:33 +0900

    22.04.11

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository.order.query;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderFlatDto { // SQL을 ORDER , ORDERITEM을 join 해서 한줄로 뽑아 올 ㅇ썌
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+}
